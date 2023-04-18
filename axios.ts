889fc68c1191dcb2a4c526633e13efb618d27519
haryan248 <haryan96@naver.com>
2023-04-18 15:17:30 +0900

    Axios 타입 분석

--- axios.ts
+++ axios.ts
@@ -1,17 +1,87 @@
-import axios, {AxiosError, AxiosResponse} from "axios";
+// 브라우저 > ky
+// 노드 > got
+
+import axios, { AxiosError, AxiosResponse } from "axios";
 
 interface Post {
-  userId: number, id: number, title: string, body: string
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
-(async() => {
+interface Created {}
+interface Data {
+  title: string;
+  body: string;
+  userId: number;
+}
+
+(async () => {
   try {
-    const response = await axios.get<Post>('https://jsonplaceholder.typicode.com/posts/1');
+    const response = await axios.get<Post, AxiosResponse<Post>>("https://jsonplaceholder.typicode.com/posts/1");
     console.log(response.data.userId);
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.log((error.response as AxiosResponse<{ message: string }>).data.message);
     }
-    const errorResponse = (error as AxiosError<{ message: string }>).response
+    const errorResponse = (error as AxiosError<{ message: string }>).response;
   }
 })();
+
+(async () => {
+  try {
+    const response = await axios.post<Created, AxiosResponse<Created>, Data>(
+      "https://jsonplaceholder.typicode.com/posts",
+      {
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }
+    );
+  } catch (error) {}
+})();
+
+// 타이핑 직접 해보기
+
+interface Config<D = any> {
+  method?: "post" | "get" | "put" | "patch" | "delete" | "head" | "options";
+  url?: string;
+  data?: D;
+}
+interface A {
+  get: <T, R = AxiosResponse<T>>(url: string) => Promise<R>;
+  post: <T, R = AxiosResponse<T>, D = any>(url: string, data: D) => Promise<R>;
+  isAxiosError: (error: unknown) => error is AxiosError;
+  (config: Config): void;
+  (url: string, config: Config): void;
+}
+
+const a: A = axios;
+(async () => {
+  try {
+    const response = await a.get<Post, AxiosResponse<Post>>("https://jsonplaceholder.typicode.com/posts/1");
+    console.log(response.data.userId);
+  } catch (error) {
+    if (a.isAxiosError(error)) {
+      console.log((error.response as AxiosResponse<{ message: string }>).data.message);
+    }
+    const errorResponse = (error as AxiosError<{ message: string }>).response;
+  }
+})();
+
+(async () => {
+  try {
+    const a: A = axios;
+    const response = await a.post<Created, AxiosResponse<Created>, Data>("https://jsonplaceholder.typicode.com/posts", {
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    });
+  } catch (error) {}
+})();
+
+// defaults 가 위치한 이유
+const a1 = new axios.Axios({ url: "" }).defaults;
+axios("");
+axios.defaults;
